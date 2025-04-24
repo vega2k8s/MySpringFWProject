9e78e7f756d83f6248e034707e7ccee5aa4cbddc
vega2k8s <vega2k8s@gmail.com>
2025-04-24 17:25:55 +0900

    junit case add

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,8 +1,11 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloJunitTest {
 
@@ -13,6 +16,11 @@ public class HelloJunitTest {
 		//2. Container 객체가 생성한 Spring Bean을 요청하기
 		Hello helloById = (Hello)context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
+		
+		//Spring Bean 의 주소비교
+		System.out.println(helloById == helloByType);
+		assertSame(helloById, helloByType);
+		
 	}
 	
 }
