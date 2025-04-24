22f1f39b5da05e63cd4f79913d58eca4dcd226d9
vega2k8s <vega2k8s@gmail.com>
2025-04-24 17:09:12 +0900

    junit case add

---
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -0,0 +1,15 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class HelloJunitTest {
+
+	@Test
+	void helloBean() {
+		//1. Container 객체생성
+		ApplicationContext context = new GenericXmlApplicationContext("");
+	}
+	
+}
