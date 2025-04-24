8221f921d5daa2f8b4ac6d8f2e82a79966fd4896
vega2k8s <vega2k8s@gmail.com>
2025-04-24 17:21:21 +0900

    junit case add

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -10,6 +10,9 @@ public class HelloJunitTest {
 	void helloBean() {
 		//1. Container 객체생성
 		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
+		//2. Container 객체가 생성한 Spring Bean을 요청하기
+		Hello helloById = (Hello)context.getBean("hello");
+		Hello helloByType = context.getBean("hello", Hello.class);
 	}
 	
 }
