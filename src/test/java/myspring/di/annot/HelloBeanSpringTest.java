771749e7493a3f5a693aac4cd3983d9b4a0e00c3
vega2k8s <vega2k8s@gmail.com>
2025-04-25 16:16:13 +0900

    annot bean test add

---
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -0,0 +1,11 @@
+package myspring.di.annot;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-di.xml")
+public class HelloBeanSpringTest {
+
+}
