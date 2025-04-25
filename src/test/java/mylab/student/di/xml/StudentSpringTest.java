d5d16da421b9726e3ed59aa0c36c6f11755b546d
vega2k8s <vega2k8s@gmail.com>
2025-04-25 14:28:23 +0900

    mylab course bean Test add

--- src/test/java/mylab/student/di/xml/StudentSpringTest.java
+++ src/test/java/mylab/student/di/xml/StudentSpringTest.java
@@ -1,11 +1,22 @@
 package mylab.student.di.xml;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:mylab-student-di.xml")
 public class StudentSpringTest {
-
+	@Autowired
+	Course course;
+	
+	@Test
+	void testCourse() {
+		assertNotNull(course);
+	}
 }
