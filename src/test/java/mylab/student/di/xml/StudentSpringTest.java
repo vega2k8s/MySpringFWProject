6a39aeef40300e728082fb8d4706f544402d42b9
vega2k8s <vega2k8s@gmail.com>
2025-04-25 14:24:25 +0900

    mylab course bean

---
+++ src/test/java/mylab/student/di/xml/StudentSpringTest.java
@@ -0,0 +1,11 @@
+package mylab.student.di.xml;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:mylab-student-di.xml")
+public class StudentSpringTest {
+
+}
