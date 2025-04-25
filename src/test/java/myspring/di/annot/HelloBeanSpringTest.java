26c3fe266a9f853c643fccc7c0ed09ea52c45d41
vega2k8s <vega2k8s@gmail.com>
2025-04-25 17:03:25 +0900

    properties file update

--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -24,5 +24,9 @@ public class HelloBeanSpringTest {
 		assertEquals("Hello 어노테이션", hello.sayHello());
 		hello.print();
 		assertEquals("Hello 어노테이션", printer.toString());
+		
+		for(String name: hello.getNames()) {
+			System.out.println(name);
+		}
 	}
 }
