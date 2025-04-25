71f3788317c2ec45fa010b3cb66a842d6c3bcb72
vega2k8s <vega2k8s@gmail.com>
2025-04-25 13:21:13 +0900

    getNames() add

--- src/test/java/myspring/di/xml/HelloConSpringTest.java
+++ src/test/java/myspring/di/xml/HelloConSpringTest.java
@@ -23,6 +23,12 @@ public class HelloConSpringTest {
 		hello.print();
 		
 		assertEquals(3, hello.getNames().size());
+		assertEquals("스프링클라우드", hello.getNames().get(2));
+		
+		//hello.getNames() ==> List<String>
+		for(String name: hello.getNames()) {
+			System.out.println(name);
+		}
 	}
 	
 }
