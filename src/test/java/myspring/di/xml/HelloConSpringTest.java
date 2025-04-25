83b6d462513b0661d17ac970e452d4abd31dbe0b
vega2k8s <vega2k8s@gmail.com>
2025-04-25 11:26:08 +0900

    spring test case update

--- src/test/java/myspring/di/xml/HelloConSpringTest.java
+++ src/test/java/myspring/di/xml/HelloConSpringTest.java
@@ -1,11 +1,24 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:hello-di.xml")
 public class HelloConSpringTest {
-
+	@Autowired
+	@Qualifier("helloC")
+	Hello hello;
+	
+	@Test
+	void helloCons() {
+		System.out.println(hello.sayHello());
+	}
+	
 }
