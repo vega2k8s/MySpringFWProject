a7a094a450440c2fd25f3ded89b466cb9b240ebf
vega2k8s <vega2k8s@gmail.com>
2025-04-25 10:39:35 +0900

    spring test case update

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,9 +1,12 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 //SpringExtension 클래스는 ApplicationContext(컨테이너)객체를 생성하는 역할을 함
 @ExtendWith(SpringExtension.class)
@@ -12,5 +15,8 @@ public class HelloSpringTest {
 	@Autowired
 	Hello hello;
 	
-	
+	@Test
+	void helloSpringBean() {
+		assertEquals("Hello 스프링", hello.sayHello());
+	}
 }
