5c6585862292d6c14bffb8adcbde1a347faaa543
vega2k8s <vega2k8s@gmail.com>
2025-04-25 10:03:34 +0900

    junit case update

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -7,12 +7,20 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 //Assertions 에 포함된 모든 static method를 import 한다.
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class HelloJunitTest {
+	ApplicationContext context;
+	
+	@BeforeEach
+	void container() {
+		//1. Container 객체생성
+		context = new GenericXmlApplicationContext("classpath:hello-di.xml");		
+	}
+	
 
 	@Test
 	void helloBean() {
-		//1. Container 객체생성
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
 		//2. Container 객체가 생성한 Hello Spring Bean을 요청하기
 		Hello helloById = (Hello)context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
@@ -22,13 +30,15 @@ public class HelloJunitTest {
 		assertSame(helloById, helloByType);
 		
 		//값 비교
+		//<property name="name" value="스프링" /> 이 설정을 테스트 한 것임
 		assertEquals("Hello 스프링", helloById.sayHello());
 		
 		helloById.print();
 		
 		//Container 객체가 생성한 StringPrinter 스프링빈을 요청하기
+		//<property name="printer" ref="strPrinter" /> 이 설정을 테스트 한 것임
 		Printer printer = context.getBean("strPrinter", Printer.class);
-		
+		assertEquals("Hello 스프링", printer.toString());
 	}
 	
 }
