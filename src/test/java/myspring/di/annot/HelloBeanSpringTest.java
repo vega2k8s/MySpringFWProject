34f5cfb06fb3c9c61433aa6e3d59ccdedde4ca9c
vega2k8s <vega2k8s@gmail.com>
2025-04-25 16:34:53 +0900

    annot bean test add

--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -2,6 +2,8 @@ package myspring.di.annot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,13 @@ public class HelloBeanSpringTest {
 	@Autowired
 	HelloBean hello;
 	
+	@Resource(name = "stringPrinter")
+	PrinterBean printer;	
+	
 	@Test
 	void helloBean() {
 		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 어노테이션", printer.toString());
 	}
 }
