01445643f65250f7eeefab2b1c57ae017eb896da
vega2k8s <vega2k8s@gmail.com>
2025-04-28 16:45:34 +0900

    UserMyBatisTest update

--- src/test/java/myspring/user/UserMyBatisTest.java
+++ src/test/java/myspring/user/UserMyBatisTest.java
@@ -1,11 +1,29 @@
 package myspring.user;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans-mybatis.xml")
 public class UserMyBatisTest {
+	@Autowired
+	DataSource dataSource;
 	
+	@Test
+	void connection() {
+		try {
+			Connection connection = dataSource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
