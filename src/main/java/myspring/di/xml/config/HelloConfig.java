dfbe206c8be34571364bc8516518f7025b1ef273
vega2k8s <vega2k8s@gmail.com>
2025-04-28 13:43:23 +0900

    HelloConfig update

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,10 +1,47 @@
 package myspring.di.xml.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
+import myspring.di.xml.StringPrinter;
 
 @Configuration
 @PropertySource(value = "classpath:values.properties")
 public class HelloConfig {
-
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public Printer stringPrinter() {
+		return new StringPrinter();
+	}
+	
+	@Bean
+	public Printer consolePrinter() {
+		return new ConsolePrinter();
+	}
+	
+	@Bean
+	public Hello hello() {
+		Hello hello = new Hello(); //alt + shift + l(엘)
+		//hello.setName("스프링")  env.getProperty("myName1") == "스프링"
+		hello.setName(env.getProperty("myName1")); //myName1=스프링
+		hello.setPrinter(stringPrinter());
+		return hello;
+	}
+	
+	@Bean
+	public List<String> namesList() {
+		return Arrays.asList("Java","Spring","Kotlin");
+	}
+	
 }
