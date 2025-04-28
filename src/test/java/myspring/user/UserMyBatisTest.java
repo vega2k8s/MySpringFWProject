2012cc16ca542f2243b88673017c2d2694bcf280
vega2k8s <vega2k8s@gmail.com>
2025-04-28 16:37:42 +0900

    UserMyBatisTest add

---
+++ src/test/java/myspring/user/UserMyBatisTest.java
@@ -0,0 +1,11 @@
+package myspring.user;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring-beans-mybatis.xml")
+public class UserMyBatisTest {
+	
+}
