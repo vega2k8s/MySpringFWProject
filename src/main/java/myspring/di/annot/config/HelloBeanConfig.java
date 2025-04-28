fa02f298361ab01626fefea8c5acdd41d5eefc7b
vega2k8s <vega2k8s@gmail.com>
2025-04-28 10:31:44 +0900

    HelloBeanConfig update

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -2,10 +2,13 @@ package myspring.di.annot.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //<context:component-scan base-package="myspring.di.annot" /> 
-@ComponentScan(basePackages = {""})
+@ComponentScan(basePackages = {"myspring.di.annot"})
+//<context:property-placeholder location="classpath:values.properties" />
+@PropertySource(value="classpath:values.properties" )
 public class HelloBeanConfig {
 
 }
