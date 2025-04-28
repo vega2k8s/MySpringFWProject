3725043bfd958d0464c059c13c9b697c124e0ee2
vega2k8s <vega2k8s@gmail.com>
2025-04-28 11:11:13 +0900

    HelloBeanConfigTest update

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -3,11 +3,14 @@ package myspring.di.annot.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import myspring.di.annot.HelloBean;
+import myspring.di.annot.PrinterBean;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -17,9 +20,17 @@ public class HelloBeanConfigTest {
 	@Autowired
 	HelloBean hello;
 	
+	@Autowired
+	@Qualifier("stringPrinter")
+	PrinterBean printer;
+	
 	@Test
 	void helloBean() {
 		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 어노테이션", printer.toString());
+		
+		assertEquals(3, hello.getNames().size());
 	}
 	
 }
